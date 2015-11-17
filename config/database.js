eb51df2bf8bb6425837bdd6f19dbe55ddf45f165
Tejas Manohar <me@tejas.io>
2015-11-17 05:30:10 -0600

    need that mongo uri env var, error if not there

--- config/database.js
+++ config/database.js
@@ -5,15 +5,16 @@
 
 import mongoose from 'mongoose'
 
+if (!process.env.MONGO_URI) throw 'Must set MONGO_URI environment variable'
 mongoose.connect(process.env.MONGO_URI)
 
 mongoose.connection.on('connected', () =>
   console.log('mongo connection open')
 )
 
-mongoose.connection.on('error', err =>
-  console.error(`MongoDB connection error: ${err}`)
-)
+mongoose.connection.on('error', err => {
+  throw `MongoDB connection error: ${err}`
+})
 
 mongoose.connection.on('disconnected', () =>
   console.error('mongo connection disconnected')
