e6cc69311287fbd73744d048816a53fcf5ae96c6
Tejas Manohar <me@tejas.io>
2015-11-17 04:30:43 -0600

    DRY-ify the ternary by avoiding double assignment

--- resources/accounts/routes/upsert.js
+++ resources/accounts/routes/upsert.js
@@ -11,6 +11,6 @@ import Account from '../model'
 
 export default async function upsert (ctx) {
   const { created, data } = await Account.upsert(ctx.request.body)
-  created ? ctx.status = 201 : ctx.status = 200
+  ctx.status = created ? 201 : 200
   ctx.body = { data }
 }
